492b80722de900f320e02584e021f9a6d251c36c
luongkhanhhd <133746367+luongkhanhhd@users.noreply.github.com>
2023-06-23 10:01:40 +0700

    KhanhLV: Use kafka for this message

---
+++ src/main/java/com/messagekafkaservice/MessageKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.messagekafkaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messagekafkaservice/MessageKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagekafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
